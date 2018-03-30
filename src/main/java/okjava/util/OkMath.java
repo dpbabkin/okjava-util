4414fa1ad21356c8ccfae76fc81ee58ec56288b0
dpbabkin <dpbabkin@gmail.com>
2018-03-30 21:51:28 +0200

    Add OkMath with average

---
+++ src/main/java/okjava/util/OkMath.java
@@ -0,0 +1,28 @@
+package okjava.util;
+
+import static okjava.util.check.Never.neverNeverCalled;
+
+import okjava.util.annotation.Utility;
+import okjava.util.check.Never;
+
+/**
+ * @author Dmitry Babkin dpbabkin@gmail.com
+ * 3/30/2018
+ * 21:50.
+ */
+@Utility
+public enum OkMath {
+    ;
+
+    OkMath(@SuppressWarnings("unused") Never never) {
+        neverNeverCalled();
+    }
+
+    public static int average(int a, int b) {
+        return (a + b) >>> 1;
+    }
+
+    public static long average(long a, long b) {
+        return (a + b) >>> 1;
+    }
+}
