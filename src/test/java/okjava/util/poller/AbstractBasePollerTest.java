f5e8b5f69ffd886fd51c691f26ab6a500d93a3ca
dpbabkin@gmail.com <dpbabkin@gmail.com>
2019-12-24 22:40:29 +0100

    price tests refactoring.

--- src/test/java/okjava/util/poller/AbstractBasePollerTest.java
+++ src/test/java/okjava/util/poller/AbstractBasePollerTest.java
@@ -22,31 +22,31 @@ abstract class AbstractBasePollerTest<P extends Poller<Long>> {
     public void testValueChanged01() throws InterruptedException {
         P poller = getPoller();
         setNewValue(1L);
-        PollerTestUtils.assertValue(poller, 1L);
+        PollerTestUtils.waitAssertValue(poller, 1L);
 
     }
 
     @Test
     public void testValueChanged02() throws InterruptedException {
         P poller = getPoller();
-        PollerTestUtils.assertValue(poller, 0L);
+        PollerTestUtils.waitAssertValue(poller, 0L);
         Thread thread = new Thread(() -> {
             setNewValue(1L);
         });
         thread.start();
-        PollerTestUtils.assertValue(poller, 1L);
+        PollerTestUtils.waitAssertValue(poller, 1L);
     }
 
     @Test
     public void testValueChanged03() throws InterruptedException {
         P poller = getPoller();
-        PollerTestUtils.assertValue(poller, 0L);
+        PollerTestUtils.waitAssertValue(poller, 0L);
 
         BlockingWaitForEvent block = BlockingWaitForEvent.createWithDefaultPoll();
         Thread thread = new Thread(() -> {
             try {
                 poller.poll();
-                PollerTestUtils.assertValue(poller, 1L);
+                PollerTestUtils.waitAssertValue(poller, 1L);
             } catch (InterruptedException e) {
                 e.printStackTrace();
                 Thread.currentThread().interrupt();
@@ -57,7 +57,7 @@ abstract class AbstractBasePollerTest<P extends Poller<Long>> {
         block.waiter(() -> thread.getState() == WAITING).second().assertTrue();
         setNewValue(1L);
         block.waiter(() -> thread.isAlive() == false).second().assertTrue();
-        PollerTestUtils.assertValue(poller, 1L);
+        PollerTestUtils.waitAssertValue(poller, 1L);
     }
 
     @Test
@@ -81,17 +81,17 @@ abstract class AbstractBasePollerTest<P extends Poller<Long>> {
 
         setNewValue(1L);
         waitForCollection.createWaiter(1).second().assertTrue();
-        PollerTestUtils.assertValue(poller, 1L);
+        PollerTestUtils.waitAssertValue(poller, 1L);
         assertThat(waitForCollection.getCollection(), contains(1L));
 
         setNewValue(3L);
         waitForCollection.createWaiter(2).second().assertTrue();
-        PollerTestUtils.assertValue(poller, 3L);
+        PollerTestUtils.waitAssertValue(poller, 3L);
         assertThat(waitForCollection.getCollection(), contains(1L, 3L));
 
         setNewValue(5L);
         waitForCollection.createWaiter(3).second().assertTrue();
-        PollerTestUtils.assertValue(poller, 5L);
+        PollerTestUtils.waitAssertValue(poller, 5L);
         assertThat(waitForCollection.getCollection(), contains(1L, 3L, 5L));
 
         thread.interrupt();
--- src/test/java/okjava/util/poller/PollerTestUtils.java
+++ src/test/java/okjava/util/poller/PollerTestUtils.java
@@ -4,6 +4,7 @@ import okjava.util.annotation.Utility;
 import okjava.util.check.Never;
 import okjava.util.poller.poller.Poller;
 
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import static okjava.util.check.Never.neverNeverCalled;
@@ -21,19 +22,20 @@ public class PollerTestUtils {
         neverNeverCalled();
     }
 
-    public static <V> void assertValue(Poller<V> poller, final V expectedValue) throws InterruptedException {
-        assertValue(poller, expectedValue, DEFAULT_WAIT_TIME, DEFAULT_TIME_UNIT);
+    public static <V> void waitAssertValue(Poller<V> poller, final V expectedValue) throws InterruptedException {
+        waitAssertValue(poller, expectedValue, DEFAULT_WAIT_TIME, DEFAULT_TIME_UNIT);
     }
 
-    public static <V> void assertValue(Poller<V> poller, final V expectedValue, long time, TimeUnit timeUnit) throws InterruptedException {
-        assertThat(poller.poll(v -> v.equals(expectedValue), time, timeUnit).isPresent(), is(true));
+    public static <V> void waitAssertValue(Poller<V> poller, final V expectedValue, long time, TimeUnit timeUnit) throws InterruptedException {
+        Optional<V> value = poller.poll(v -> v.equals(expectedValue), time, timeUnit);
+        assertThat(value.isPresent(), is(true));
     }
 
-    public static <V> void assertValue(ValueHolder<V> valueHolder, final V expectedValue) throws InterruptedException {
-        assertValue(valueHolder, expectedValue, DEFAULT_WAIT_TIME, DEFAULT_TIME_UNIT);
+    public static <V> void waitAssertValue(ValueHolder<V> valueHolder, final V expectedValue) throws InterruptedException {
+        waitAssertValue(valueHolder, expectedValue, DEFAULT_WAIT_TIME, DEFAULT_TIME_UNIT);
     }
 
-    public static <V> void assertValue(ValueHolder<V> valueHolder, final V expectedValue, long time, TimeUnit timeUnit) throws InterruptedException {
-        assertValue(valueHolder.getPoller(), expectedValue, time, timeUnit);
+    public static <V> void waitAssertValue(ValueHolder<V> valueHolder, final V expectedValue, long time, TimeUnit timeUnit) throws InterruptedException {
+        waitAssertValue(valueHolder.getPoller(), expectedValue, time, timeUnit);
     }
 }
